35277c24024a910cabaa1e980a87950636c6043d
khaledalfallati00 <khaledalfallati0110@gmail.com>
2025-12-29 15:59:39 +0300

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const fetch = require('node-fetch');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-const GEMINI_API_KEY = "AIzaSyDEXilH-rO39SgNIw3O-mPILl5jOMJcnZY";
+const GEMINI_API_KEY = "AIzaSyC3rmVW31SI8-BAcOVfhgsEivaEQssgQKs";
 const GEMINI_URL = `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${GEMINI_API_KEY}`;
 
 app.use(cors());
